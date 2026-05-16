82fe4d87485714905f49c57e53a0184ae3676514
Ahmed Missouri <ahmedmissouri@icloud.com>
2026-05-16 11:15:37 +0100

    updated nextjs config and added shadcn components

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+      },
+      {
+        protocol: "https",
+        hostname: "images.metahub.space",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.discordapp.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
