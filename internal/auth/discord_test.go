58a9ff75194e1ce7a73308a36b3cb85364ec0b21
Ahmed Missouri <ahmedmissouri@icloud.com>
2026-09-05 04:15:05 +0100

    fixed error for fmt.Fprint in Discord user access test

--- internal/auth/discord_test.go
+++ internal/auth/discord_test.go
@@ -22,17 +22,17 @@ func TestFetchDiscordUserHasAccess(t *testing.T) {
 		mux := http.NewServeMux()
 		mux.HandleFunc("/oauth2/token", func(w http.ResponseWriter, _ *http.Request) {
 			w.Header().Set("Content-Type", "application/json")
-			fmt.Fprint(w, `{"access_token":"tok","token_type":"bearer"}`)
+			_, _ = fmt.Fprint(w, `{"access_token":"tok","token_type":"bearer"}`)
 		})
 		mux.HandleFunc("/users/@me", func(w http.ResponseWriter, _ *http.Request) {
-			fmt.Fprint(w, `{"id":"42","username":"u","avatar":"a"}`)
+			_, _ = fmt.Fprint(w, `{"id":"42","username":"u","avatar":"a"}`)
 		})
 		mux.HandleFunc("/users/@me/guilds", func(w http.ResponseWriter, r *http.Request) {
 			if r.Header.Get("Authorization") != "Bearer tok" {
 				w.WriteHeader(http.StatusUnauthorized)
 				return
 			}
-			fmt.Fprint(w, tc.guilds)
+			_, _ = fmt.Fprint(w, tc.guilds)
 		})
 		srv := httptest.NewServer(mux)
 
